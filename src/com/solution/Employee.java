24969d496945a8c1dcde283bd1f272d2d07a2729
dosskannan <dosskannan17@gamil.com>
2022-02-15 20:16:48 +0530

    files upload

---
+++ src/com/solution/Employee.java
@@ -0,0 +1,15 @@
+package com.solution;
+
+public class Employee {
+	
+	private void empid() {
+	
+		System.out.println("kanna");
+
+	}
+
+	public static void main(String[] args) {
+		Employee E = new Employee();
+		E.empid();
+	}
+}
